d70c0b30aba236b2644b4bc212b7f6e41f9050ea
“Quvonchbek5436” <quvonchbek5436@gmail.com>
2022-11-08 08:56:40 +0500

    QVC

--- pages/login.tsx
+++ pages/login.tsx
@@ -60,60 +60,10 @@ const Login = () => {
         }
     }
 
-    // const handleSubmit = (event:any) => {
-    //     event.preventDefault();
-    //     const data = new FormData(event.currentTarget);
-    //     console.log({
-    //         email: data.get('email'),
-    //         password: data.get('password'),
-    //     });
-    // };
+
 
     return (
-        // <div
-        //     style={{
-        //         width: '40%',
-        //         margin: 'auto',
-        //     }}
-        // >
-        //     <h1 className="text-center my-3 ">Login</h1>
-        //     <Form onSubmit={handleLogin}>
-        //         <Form.Group className="mb-3" controlId="formBasicEmail">
-        //             <Form.Label>Email address</Form.Label>
-        //             <Form.Control
-        //                 onChange={(e: any) =>
-        //                     setData({
-        //                         ...data,
-        //                         email: e.target.value,
-        //                     })
-        //                 }
-        //                 value={data.email}
-        //                 required
-        //                 type="email"
-        //                 placeholder="Enter email"
-        //             />
-        //         </Form.Group>
-        //
-        //         <Form.Group className="mb-3" controlId="formBasicPassword">
-        //             <Form.Label>Password</Form.Label>
-        //             <Form.Control
-        //                 onChange={(e: any) =>
-        //                     setData({
-        //                         ...data,
-        //                         password: e.target.value,
-        //                     })
-        //                 }
-        //                 value={data.password}
-        //                 required
-        //                 type="password"
-        //                 placeholder="Password"
-        //             />
-        //         </Form.Group>
-        //         <Button variant="primary" type="submit">
-        //             Login
-        //         </Button>
-        //     </Form>
-        // </div>
+
         <>
             <ThemeProvider theme={theme}>
                 <Container component="main" maxWidth="xs">
